9fde89f407644469db5cc5700786937eef37948c
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-04-04 16:42:40 +0530

    targer _balnk1

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -24,13 +24,13 @@ const HomePage = () => {
           {/* <Link to="https://www.linkedin.com/in/odedara-ranjeet-2a3549262/" target="_blank">
 
           </Link> */}
-          <Link to="/">
+      
             <a href="https://www.linkedin.com/in/odedara-ranjeet-2a3549262/" target="_blank" rel="noopener noreferrer">
               <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl social-logos">
                 <img src={linkedin} alt="linkedin" />
               </div>
                </a>
-          </Link>
+         
           <Link to="https://github.com/Ranjeet0062" target="_blank">
             <div className=" p-2 bg-white rounded-2xl mx-2 shadow-xl social-logos">
               <img src={github} alt="github" />
