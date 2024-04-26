802598f7b92515fad66d8bed19d589ca9fc8f860
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-04-26 23:27:56 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-// import AboutMe from "./pages/aboutMe/AboutMe";
-// import HomePage from "./pages/home/HomePage";
-// import "./pages/cursor.css";
-// import Skills from "./pages/skills/Skills";
-// import ProjectsPage from "./pages/projects/ProjectsPage";
-// import ContactMe from "./pages/contact/ContactMe";
 import { Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage";
-// import Resume from "./pages/resume/Resume.jsx"
+import Resume from "./pages/Resume.jsx"
 function App() {
   return (
     <div className="" style={{ backgroundColor: 'rgb(20, 20, 32)' }}>
       <Routes>
         <Route path="/" element={<MainPage/>}></Route>
-        {/* <Route path="/resume" element={<Resume/>}></Route> */}
+        <Route path="/resume" element={<Resume/>}></Route>
       </Routes>
     </div>
   );
